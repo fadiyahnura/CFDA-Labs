60ed03f1bcedcba196d3e40c188eb65c95effbc8
fadiyah nur afifah <fadiyahna260104@gmail.com>
2026-07-24 10:17:53 +0700

    Delete managament spd directory

--- managament spd/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace managament_spd.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- managament spd/Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ManagementSPD.Services
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string to, string subject, string body);
-    }
-}
